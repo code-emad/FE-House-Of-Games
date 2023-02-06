ce12330639affda8549d885de8d45f148cf33ad7
code-emad <emadul.islam@hotmail.co.uk>
2023-02-06 15:58:34 +0000

    adding a timestamp to each review

--- src/Components/ReviewCard.jsx
+++ src/Components/ReviewCard.jsx
@@ -5,7 +5,7 @@ export default function ReviewCard ({reviews}) {
             {reviews.map((review) => { return (
                 <li key={review.review_id}>
                     <h3>{review.review_id}. {review.title}</h3>
-                    <p>User: {review.owner}</p>
+                    <p>User: {review.owner} @ {review.created_at.slice(0,16).replace("T", " ")}</p>
                     <img src={review.review_img_url} height="100px"/>
                     <p>Review: {review.review_body}</p>
                     <p>Designer: {review.designer}</p>
