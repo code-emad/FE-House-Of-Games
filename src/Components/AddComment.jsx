e360ba6325a70d92e6522f9278c5d87ce216c795
code-emad <emadul.islam@hotmail.co.uk>
2023-02-09 16:01:53 +0000

    updated addComment to now optimistically render comments

--- src/Components/AddComment.jsx
+++ src/Components/AddComment.jsx
@@ -9,31 +9,25 @@ export default function AddComment({ review_id, comments, setComments }) {
     event.preventDefault();
 
     const username = "tickle122";
-
     setDisableButton(true);
-    //update UI
-    console.log(comments, "from addcomment 1");
     setComments((currComments) => {
       return [
-        ...currComments,
         {
           created_at: new Date().toISOString(),
           votes: 0,
           author: username,
           body: newComment,
-          comment_id: Date.now()
+          comment_id: Date.now(),
         },
+        ...currComments,
       ];
     });
-    console.log(comments, " from addcomment 2");
-
-    //making the request
-    // addComment(review_id, newComment)
-    //   .then((data) => {
-    //     setNewComment("");
-    //     setDisableButton(false);
-    //   })
-    //   .catch((err) => console.log(err));
+    addComment(review_id, newComment)
+      .then((data) => {
+        setNewComment("");
+        setDisableButton(false);
+      })
+      .catch((err) => console.log(err));
   }
   return (
     <section>
