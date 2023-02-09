11c3e06d7e963bfc0a95dca3a99e23c93aa3d134
Emad <118465790+code-emad@users.noreply.github.com>
2023-02-09 11:10:46 +0000

    Revert "functionality for add comment on to a review is done"

--- src/Components/SingleReview.jsx
+++ src/Components/SingleReview.jsx
@@ -4,7 +4,6 @@ import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import Comments from './Comments'
 import Votes from './Votes'
-import AddComment from "./AddComment";
 
 export default function SingleReview() {
   const navigate = useNavigate();
@@ -16,7 +15,7 @@ export default function SingleReview() {
       getReviewById(review_id).then((review) => {
         setReview(review);
       });
-  }, [review_id,  <AddComment />]);
+  }, [review_id]);
 
   const searchedReview = !review.hasOwnProperty("review_body") ? (<p></p>) 
   : (
